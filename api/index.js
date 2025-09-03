5f3720b8cdd41ce005edf1a69a966a1f7a5c9024
MichaelOnyoin <moonyoin@gmail.com>
2025-09-03 12:01:24 +0300

    Trying 3

--- api/index.js
+++
@@ -1,9 +0,0 @@
-    // api/index.js
-    const express = require("express");
-    const app = express();
-
-    app.get("/", (req, res) => {
-        res.send("Hello from Express on Vercel!");
-    });
-
-    module.exports = app; // Export the app for Vercel
