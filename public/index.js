1cec20cf85dd41c79fb801f4be395831af7c2b89
MichaelOnyoin <moonyoin@gmail.com>
2025-09-03 14:10:38 +0300

    Changes to be committed: 	new file:   public/index.html 	renamed:    index.js -> public/index.js

--- public/index.js
+++ public/index.js
@@ -53,7 +53,7 @@ module.exports = function main (options, cb) {
   // Alternativly, you could setup external log handling for startup
   // errors and handle them outside the node process.  I find this is
   // better because it works out of the box even in local development.
-  require('./routes')(app, opts)
+  require('../routes')(app, opts)
 
   // Common error handlers
   app.use(function fourOhFourHandler (req, res, next) {
@@ -89,7 +89,7 @@ module.exports = function main (options, cb) {
   })
 }
 
-require('./')({
+require('.')({
   port: 8000,
   host: 'localhost'
 })
